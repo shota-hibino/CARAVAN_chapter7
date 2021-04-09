b4ae1dc9873c6fbfdc1e766cf178ed5b226c4815
shota-hibino <yamoushou@gmail.com>
2021-04-09 01:27:10 +0000

    [Update]index.html.erb file create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # indexからdestroyまでの7つのアクションを設定
+  resources :blogs
 end
